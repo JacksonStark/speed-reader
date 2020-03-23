7b06f2fcdcdb83a56f8218f1a91d1ef2fc87e068
Jackson Stark <jacksonstark77@hotmail.com>
2020-03-23 12:15:11 -0600

    implement chunker helper function

--- src/helpers/chunker.js
+++ src/helpers/chunker.js
@@ -1,3 +1,15 @@
 export default function chunker(array, chunkSize) {
-  
+  let chunks = [[]];
+
+  array.forEach(word => {
+    if (chunks[chunks.length - 1].length !== chunkSize) {
+      // if last element hasnt hit the chunkSize limit, add word to last element (chunk)
+      chunks[chunks.length - 1].push(word);
+    } else {
+      // else if the last element has 3 elements already, start new chunk of words
+      chunks.push([word]);
+    }
+  });
+
+  return chunks;
 }
