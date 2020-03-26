a6b29edda375528123f132cae57e1de983e44f13
Jackson Stark <jacksonstark77@hotmail.com>
2020-03-26 02:27:35 -0600

    implement useKeyPress custom hook

--- src/components/SpeedReader.js
+++ src/components/SpeedReader.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import './SpeedReader.scss'
 import useVisualMode from '../hooks/useVisualMode.js';
+import useKeyPress from '../hooks/useKeyPress.js';
+
 
 
 export default function SpeedReader({ trimmedData, readingSpeed, chunkSize, setReading }) {
-  const [displayData, setDisplayData] = useState(trimmedData)
-  const [displayWords, setDisplayWords] = useState('')
+  // const [displayData, setDisplayData] = useState(trimmedData)
+  // const [displayWords, setDisplayWords] = useState('')
+  const { displayChunk, forward, back, finished } = useVisualMode(trimmedData)
+  const { pausePress, backPress, forwardPress, setBackPress, setForwardPress } = useKeyPress()
 
-  const {} = useVisualMode(trimmedData)
   
   // runs every render, where it'll take off the first five words
   useEffect(() => {
@@ -15,24 +18,33 @@ export default function SpeedReader({ trimmedData, readingSpeed, chunkSize, setR
     // amount of time for each word, then multiply it by chunkSize for our timeoutTime
     let wordTime = 60000 / Number(readingSpeed);
     let timeoutTime = chunkSize * wordTime;
+    let timeout;
     
-    // let data = displayData
-    
-    if (displayData.length === 0) {
+    if (finished) {
       setTimeout(() => setReading(false), timeoutTime)
     }
 
-    
-    // setDisplayState to next five words...
-    let timeout = setTimeout(() => {
-      // grab first three words
-      let chunkWords = displayData.slice(0, chunkSize);
-      console.log({chunkWords})
 
-      let remainingData = displayData <= chunkSize ? displayData : displayData.slice(chunkSize)
-      setDisplayData(remainingData);
-      setDisplayWords(chunkWords.join(' '));
-    }, timeoutTime);
+
+    if (pausePress) {
+      console.log("in SpeedReader.js pausePress IF")
+      // if reading is paused allow forward and back (time-travel) functionality
+      if (backPress) {
+        back();
+        setBackPress(false);
+      }
+      if (forwardPress) {
+        forward();
+        setForwardPress(false);
+      }
+
+    } else {
+      // else continue with speed reading timeouts
+      timeout = setTimeout(() => {
+        forward()
+        // setDisplayWords(chunk.join(' '));
+      }, timeoutTime);
+    }
 
     return () => clearTimeout(timeout)
 
@@ -41,7 +53,7 @@ export default function SpeedReader({ trimmedData, readingSpeed, chunkSize, setR
 
   return (
     <div className="display-words">
-      {displayWords}
+      {displayChunk}
     </div>
   )
 }
