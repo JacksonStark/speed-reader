588a4f79ccd61cb9fc25c8334539865b144a2373
Jackson Stark <jacksonstark77@hotmail.com>
2020-04-09 17:37:08 -0600

    move bulk of logic and useeffecr into press hook

--- src/components/SpeedReader.js
+++ src/components/SpeedReader.js
@@ -1,53 +1,15 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useContext } from 'react'
 import { ReaderContext } from './ReadingData'
 import './SpeedReader.scss'
-import useVisualMode from '../hooks/useVisualMode.js';
 import useKeyPress from '../hooks/useKeyPress.js';
 
 
 
-export default function SpeedReader({ trimmedData, readingSpeed, chunkSize, setReading }) {
-  const { displayChunk, forward, back, finished } = useVisualMode(trimmedData)
-  const { pausePress, backPress, forwardPress, setBackPress, setForwardPress } = useKeyPress()
+export default function SpeedReader({ readerData, timeoutTime }) {
   const { state, dispatch } = useContext(ReaderContext);
+  const { displayChunk } = useKeyPress(readerData, timeoutTime, state, dispatch)
 
   
-  // runs every render, where it'll take off the first five words
-  useEffect(() => {
-    
-    // amount of time for each word, then multiply it by chunkSize for our timeoutTime
-    let wordTime = 60000 / Number(readingSpeed);
-    let timeoutTime = chunkSize * wordTime;
-    let timeout;
-    
-    if (finished) {
-      setTimeout(() => dispatch({ type: "SET_READING_STATUS", value: false }), timeoutTime)
-    }
-
-    if (pausePress) {
-      console.log("in SpeedReader.js pausePress IF")
-      // if reading is paused allow forward and back (time-travel) functionality
-      if (backPress) {
-        back();
-        setBackPress(false);
-      }
-      if (forwardPress) {
-        forward();
-        setForwardPress(false);
-      }
-
-    } else {
-      // else continue with speed reading timeouts
-      timeout = setTimeout(() => {
-        forward()
-      }, timeoutTime);
-    }
-
-    return () => clearTimeout(timeout)
-
-  })
-
-
   return (
     <div className="display-words">
       {displayChunk}
