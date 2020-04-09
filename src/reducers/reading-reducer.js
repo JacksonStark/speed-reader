abe7c3aa400469d72ae7c5e6db8303dd4e5733d8
Jackson Stark <jacksonstark77@hotmail.com>
2020-04-09 17:52:06 -0600

    fix initial reducer data to be more relevant

--- src/reducers/reading-reducer.js
+++ src/reducers/reading-reducer.js
@@ -1,14 +1,9 @@
 export const initialState = {
-  rawData: `According to the US Centers for Disease Control and Prevention, the virus spreads from person to person when people are within about 6 feet of each other "through respiratory droplets produced when an infected person coughs or sneezes."
-  Fineberg told CNN this is true, but that research shows that aerosolized droplets produced by talking or possibly even by just breathing can also spread the virus.
-  His letter explains that research at a hospital in China shows the virus can be suspended in the air when doctors and nurses remove protective gear, or when floors are cleaned, or when staff move around.
-  Research by the University of Nebraska shows that genetic material from the virus was found in patients' rooms more than 6 feet away from the patients, according to the letter.
-  Fineberg said it's possible that aerosolized coronavirus droplets can hang in the air and potentially infect someone who walks by later.
-  He added, however, that coronavirus is not as infectious as measles or tuberculosis.
-  How long coronavirus lingers in the air depends on several factors, including how much virus an infected individual puts out when breathing or talking, and also on the amount of circulation in the air, he said."`,
+  rawData: `To get started, just copy and paste text that you would like to read in this text box. \n
+StarkReader will help you read faster by flashing words at you using what is called a Rapid Serial Visual Presentation.`,
   readerData: [],
   readingSpeed: 300,
-  excludedWords: 'Control and Prevention, the virus spreads from person to person when people are',
+  excludedWords: '',
   chunkSize: 3,
   readingStatus: false
 }
