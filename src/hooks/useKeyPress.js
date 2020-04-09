2e46f0d3e606135e8247013dd16cb3ce4cbcf64f
Jackson Stark <jacksonstark77@hotmail.com>
2020-04-09 17:38:35 -0600

    consolidate live running logic (useffect) in hook

--- src/hooks/useKeyPress.js
+++ src/hooks/useKeyPress.js
@@ -1,32 +1,50 @@
 import { useState, useEffect } from "react";
+import useVisualMode from '../hooks/useVisualMode.js';
 
-export default function useKeyPress() {
-  const [pausePress, setPausePress] = useState(false);
-  const [backPress, setBackPress] = useState(false);
-  const [forwardPress, setForwardPress] = useState(false);
 
-  function pressHandler({ key }) {
+export default function useKeyPress(readerData, timeoutTime, state, dispatch) {
+  const { displayChunk, forward, back, restart, finished } = useVisualMode(readerData)
+  const [paused, setPaused] = useState(false);
+  const { readingSpeed } = state
 
-    console.log({key, pausePress, backPress})
-
-    if (key === " ") {
-      setPausePress(!pausePress);
-    } 
-    
-    if (pausePress && key === "ArrowLeft") {
-      setBackPress(true);
-    } 
-    
-    if (pausePress && key === "ArrowRight") {
-      setForwardPress(true);
-    }
-  }
-
+  // runs whenever deps change, where it'll take off the first five words
   useEffect(() => {
+    
+    function pressHandler({ key }) {
+  
+      if (key === " ") setPaused(!paused);
+      if (key === "r") restart();
+      // if reading paused... allow "time travel" controls
+      if (paused && key === "ArrowLeft") back();
+      if (paused && key === "ArrowRight") forward();
+
+      if (key === "ArrowUp") {
+        dispatch({ type: "SET_READING_SPEED", value: readingSpeed + 25 })
+      }
+      if (key === "ArrowDown") {
+        dispatch({ type: "SET_READING_SPEED", value: readingSpeed - 25 })
+      }
+      if (key === "n") {
+        setTimeout(() => dispatch({ type: "SET_READING_STATUS", value: false }))
+      }
+    }
+
+    let timeout;
+
+    if (finished) {
+      setTimeout(() => dispatch({ type: "SET_READING_STATUS", value: false }), timeoutTime)
+    }
+    
+    // continue with speed reading timeouts if not paused
+    if (!paused) timeout = setTimeout(() => forward(), timeoutTime);
+
     window.addEventListener("keydown", pressHandler);
 
-    return () => window.removeEventListener("keydown", pressHandler);
-  }); // Empty array ensures that effect is only run on mount and unmount
+    return () => {
+      window.removeEventListener("keydown", pressHandler);
+      clearTimeout(timeout);
+    }
+  }); 
 
-  return { pausePress, backPress, setBackPress, forwardPress, setForwardPress };
+  return { displayChunk };
 }
