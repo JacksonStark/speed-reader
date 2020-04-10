621927f797e427a7dfac4b4e3b1e835851803974
Jackson Stark <jacksonstark77@hotmail.com>
2020-04-09 19:44:02 -0600

    enable timetravel while live instead of just pause

--- src/hooks/useKeyPress.js
+++ src/hooks/useKeyPress.js
@@ -4,10 +4,10 @@ import useVisualMode from '../hooks/useVisualMode.js';
 
 export default function useKeyPress(readerData, timeoutTime, state, dispatch) {
   const { displayChunk, forward, back, restart, finished } = useVisualMode(readerData)
-  const [paused, setPaused] = useState(false);
+  const [ paused, setPaused ] = useState(false);
   const { readingSpeed } = state
 
-  // runs whenever deps change, where it'll take off the first five words
+  // runs on every render, where it'll take display the next set of five words
   useEffect(() => {
     
     function pressHandler({ key }) {
@@ -15,8 +15,8 @@ export default function useKeyPress(readerData, timeoutTime, state, dispatch) {
       if (key === " ") setPaused(!paused);
       if (key === "r") restart();
       // if reading paused... allow "time travel" controls
-      if (paused && key === "ArrowLeft") back();
-      if (paused && key === "ArrowRight") forward();
+      if (key === "ArrowLeft") back();
+      if (key === "ArrowRight") forward();
 
       if (key === "ArrowUp") {
         dispatch({ type: "SET_READING_SPEED", value: readingSpeed + 25 })
