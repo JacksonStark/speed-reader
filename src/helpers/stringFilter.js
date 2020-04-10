17907153e4698b88bd5fcb5191b6b758a7337358
Jackson Stark <jacksonstark77@hotmail.com>
2020-04-09 19:41:12 -0600

    refactor to use recursion instead of regex

--- src/helpers/stringFilter.js
+++ src/helpers/stringFilter.js
@@ -1,16 +1,14 @@
+
+// DEF: filters data string of excluded words
+// IN: stringToFilter (array), excludedWords (array), default index (0)
+// OUT: filtered string free of excluded words
+
 export default function stringFilter(stringToFilter, excludedWords, index = 0) {
 
-  let wordsToRemove = excludedWords.split(' ')
-  let re = new RegExp(wordsToRemove[index], 'g')
-  let filteredString = stringToFilter.replace(re, '')
+  let filteredString = stringToFilter.filter(word => word !== excludedWords[index])
 
-  if (index === stringToFilter.length - 1) {
-    let filterResult = filteredString.split(' ').filter(x => x !== "")
-
-    console.log({filterResult})
-    
-    return filterResult
-
+  if (index === excludedWords.length - 1) {
+    return filteredString;
   } else {
     return stringFilter(filteredString, excludedWords, index + 1);
   }
