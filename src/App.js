61c1bf69e095439666f8d98bdcea99bdfb3f4db5
Jackson Stark <jacksonstark77@hotmail.com>
2020-04-15 13:33:36 -0600

    Move NavBar out of app JS and into reading data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 import ReadingData from './components/ReadingData.js';
-import NavBar from './components/NavBar.js';
 
 function App() {
 
   return (
     <div className="App">
-
-      <NavBar />
 
       <ReadingData />
 
